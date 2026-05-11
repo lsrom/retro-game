682592c8941a60763f16a303660ad76881a2175c
lsrom <lukas.srom@gmail.com>
2026-05-11 17:41:27 +0200

    Add trader feature flag

--- src/main/java/com/github/retro_game/retro_game/controller/TraderController.java
+++ src/main/java/com/github/retro_game/retro_game/controller/TraderController.java
@@ -8,6 +8,7 @@ import com.github.retro_game.retro_game.service.UserService;
 import com.github.retro_game.retro_game.service.exception.NotEnoughResourcesException;
 import com.github.retro_game.retro_game.service.exception.WrongTradeAmountException;
 import com.github.retro_game.retro_game.service.exception.WrongTradedResourceException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -23,10 +24,14 @@ import javax.validation.Valid;
 public class TraderController {
   private final UserService userService;
   private final TraderService traderService;
+  private final boolean allowTrader;
 
-  public TraderController(UserService userService, TraderService traderService) {
+  public TraderController(UserService userService,
+                          TraderService traderService,
+                          @Value("${retro-game.allow-trader}") boolean allowTrader) {
     this.userService = userService;
     this.traderService = traderService;
+    this.allowTrader = allowTrader;
   }
 
   @GetMapping("/trader")
@@ -36,10 +41,14 @@ public class TraderController {
                        @RequestParam(required = false) String error,
                        Model model) {
     var ctx = userService.getCurrentUserContext(bodyId);
-    var offer = traderService.getCurrentOffer();
 
     model.addAttribute("bodyId", bodyId);
     model.addAttribute("ctx", ctx);
+    if (!allowTrader) {
+      model.addAttribute("error", "TRADER_FEATURE_DISABLED");
+      return "trader";
+    }
+    var offer = traderService.getCurrentOffer();
     model.addAttribute("error", error);
     model.addAttribute("tradedResource", offer.getTradedResource());
     model.addAttribute("tradedResourceKey", offer.getTradedResourceKey());
@@ -57,6 +66,10 @@ public class TraderController {
   @PreAuthorize("hasPermission(#form.body, 'ACCESS')")
   @Activity(bodies = "#form.body")
   public String doTrade(@Valid @ModelAttribute("form") TradeForm form, BindingResult bindingResult) {
+    if (!allowTrader) {
+      return "redirect:/trader?body=" + form.getBody() + "&error=TRADER_FEATURE_DISABLED";
+    }
+
     if (bindingResult.hasErrors()) {
       return "redirect:/trader?body=" + form.getBody() + "&error=INVALID_TRADE_FORM";
     }
