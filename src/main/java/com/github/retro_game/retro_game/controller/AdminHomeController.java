19d21b01220127fb9181846ac40a860bd3abc41b
lsrom <lukas.srom@gmail.com>
2026-06-18 23:04:14 +0200

    Link to overview from admin panel

--- src/main/java/com/github/retro_game/retro_game/controller/AdminHomeController.java
+++ src/main/java/com/github/retro_game/retro_game/controller/AdminHomeController.java
@@ -1,12 +1,28 @@
 package com.github.retro_game.retro_game.controller;
 
+import com.github.retro_game.retro_game.repository.BodyRepository;
+import com.github.retro_game.retro_game.security.CustomUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminHomeController {
+  private final BodyRepository bodyRepository;
+
+  public AdminHomeController(BodyRepository bodyRepository) {
+    this.bodyRepository = bodyRepository;
+  }
+
   @GetMapping("/admin/")
   public String home() {
     return "admin-home";
+  }
+
+  @GetMapping("/admin/game")
+  public String gameOverview() {
+    return bodyRepository.findIdsByUserIdOrderById(CustomUser.getCurrentUserId()).stream()
+        .findFirst()
+        .map(bodyId -> "redirect:/overview?body=" + bodyId)
+        .orElse("redirect:/");
   }
 }
