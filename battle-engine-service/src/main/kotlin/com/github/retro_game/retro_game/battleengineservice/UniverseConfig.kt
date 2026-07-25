fcdb72504244fa0ea888c881cc68bf864a8b7448
lsrom <lukas.srom@gmail.com>
2026-07-25 11:40:59 +0200

    Add moonshot config to universe config

--- battle-engine-service/src/main/kotlin/com/github/retro_game/retro_game/battleengineservice/UniverseConfig.kt
+++ battle-engine-service/src/main/kotlin/com/github/retro_game/retro_game/battleengineservice/UniverseConfig.kt
@@ -3,4 +3,13 @@ package com.github.retro_game.retro_game.battleengineservice
 data class UniverseConfig(
     val fleetToDebris: Double = 0.3,
     val defenseToDebris: Double = 0.0,
+    val moonshotConfig: MoonshotConfig = MoonshotConfig(
+        debrisPerUnit = 100_000,
+        maxPercent = 20
+    ),
+)
+
+data class MoonshotConfig(
+    val debrisPerUnit: Long,
+    val maxPercent: Int
 )
